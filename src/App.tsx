df6e07f34a595c71c6cebe59588c8b6d63074329
hostmateio <143047867+hostmateio@users.noreply.github.com>
2024-12-05 08:40:30 +1000

    Initial dashboard setup with Vite and React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ErrorDashboard from './ErrorDashboard'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ErrorDashboard />
+    </div>
+  )
+}
+
+export default App
